4422c63cbcdccc0ec8db7540cc4caa159327ec56
AndreasJoehr <andreas.joehr@acceler-8.ch>
2025-07-22 15:32:43 +0200

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,4 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
+test('App rendert ohne Fehler', () => {
+  // Testet, ob die App-Komponente ohne Fehler rendert
+  // (Passe ggf. an deine App an)
+  render(<App />);
+});
